d163dd0f10aca93c7e6de20fb66651819efb3877
mercury <122353133+kostya1102m@users.noreply.github.com>
2026-01-21 22:52:57 +0900

    change path

--- amazonstore/store/urls.py
+++ amazonstore/store/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.product_management, name='product_management'),
     path('products/add/', views.add_product, name='add_product'),
-    path('products/<str:product_id>/edit/', views.edit_product, name='edit_product'),
-    path('products/<str:product_id>/delete/', views.delete_product, name='delete_product'),
+    path('products/edit/<str:product_id>/', views.edit_product, name='edit_product'),
+    path('products/delete/<str:product_id>/', views.delete_product, name='delete_product'),
 ]
