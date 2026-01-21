fe6aa3e62823e5f6b38491cc9b9dce452664a3f6
mercury <122353133+kostya1102m@users.noreply.github.com>
2026-01-21 21:59:52 +0900

    add urls for product form

--- amazonstore/amazonstore/urls.py
+++ amazonstore/amazonstore/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from store import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.index, name='index'),
+    path('', include('store.urls'))
+    
 ]
--- amazonstore/store/urls.py
+++ amazonstore/store/urls.py
@@ -5,4 +5,8 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('products/', views.product_management, name='product_management'),
+    path('products/add/', views.add_product, name='add_product'),
+    path('products/<str:product_id>/edit/', views.edit_product, name='edit_product'),
+    path('products/<str:product_id>/delete/', views.delete_product, name='delete_product'),
 ]
