1699b0044eaf09f19c3471baab19e1ee326488bf
mercury <122353133+kostya1102m@users.noreply.github.com>
2026-01-21 12:03:22 +0900

    add customer model

--- amazonstore/store/models.py
+++ amazonstore/store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Customer(models.Model):
+    CustomerID = models.CharField(max_length=20, primary_key=True)
+    CustomerName = models.CharField(max_length=255)
+    City = models.CharField(max_length=255)
+    State = models.CharField(max_length=255)
+    Country = models.CharField(max_length=255)
+
+
+    class Meta:
+        db_table = 'Customers'
+        ordering = ['CustomerName']
+    
+    def __str__(self):
+        return f"{self.CustomerName ({self.CustomerID})}"
